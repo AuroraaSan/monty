a492e8f74f98650fdfa7c8518f44c5c368328fc1
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-21 01:06:39 +0300

    vs

--- error_handlingfunc.c
+++ error_handlingfunc.c
@@ -11,11 +11,88 @@ void error_str(int code, ...)
 	int num;
 
 	start_v(argg, code);
-	num = arg_value(argg, int);
+	num = va_arg(argg, int);
 	switch (code)
 	{
 		case 10:
-			fprintf(stderr, )
+			fprintf(stderr, "L%d: value out of range\n", num);
+			break;
+		case 11:
+			fprintf(stderr, "L%d: stack empty\n", num);
+			break;
+		default:
+			break;
 	}
+	n_free();
+	exit(EXIT_FAILURE);
+}
+/**
+ * err1 - print error message according to each case
+ * @code: error code
+*/
+void err1(int code, ...)
+{
+	 va_list agg;
+	 char *pp;
+	 int num;
 
+	 va_start(agg, code);
+	 switch(code)
+	 {
+		case 1:
+			fprintf(stderr, "Usage: header file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error, can't open file %s\n", va_arg(agg, char *));
+			break;
+		case 3:
+			num = va_arg(agg, int);
+			pp = va_arg(agg, char *);
+			fprintf(stderr, "L%d: instruction is unkown %s\n", num, pp);
+			break;
+		case 4:
+			fprintf(stderr, "malloc failed error\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push int\n", va_arg(agg, int));
+			break;
+		default:
+			break;
+	 }
+	 n_free();
+	 exit(EXIT_FAILURE);
+}
+/**
+ * err2 - handles error by printing appropriate messages
+ * code: error code
+ * Return: void
+*/
+void err2(int code, ...)
+{
+	va_list agg;
+	char *pp;
+	int num;
+
+	va_start(agg, code);
+	switch (code)
+	{
+	case 6:
+		fprintf(stderr, "L%d: stack empty can't pint\n", va_arg(agg, int));
+		break;
+	case 7:
+		fprintf(stderr, "L%d: empty stack can't pop", num, pp);
+		break;
+	case 8:
+		num = va_arg(agg, unsigned int);
+		pp = va_arg(agg, char *);
+		fprintf(stderr, "L%d: stack too short, can't %s\n", num, pp);
+		break;
+	case 9:
+		fprintf(stderr, "L%d: error divided by zero", va_arg(agg, unsigned int));
+		break;
+	default:
+		break;
+	}
+	n_free();
+	exit(EXIT_FAILURE);
 }
