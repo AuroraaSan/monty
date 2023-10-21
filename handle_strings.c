d84a2cc7ab6061814036716d231fc3f36e1c27ba
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-21 15:27:10 +0300

    compile

--- handle_strings.c
+++ handle_strings.c
@@ -66,3 +66,54 @@ void character_print(stack_t **stk, unsigned int num_line)
 	/*else, print the character of the provided ascii*/
 	printf("%c\n", asc);
 }
+/**
+ * rotate_l - change pointer from the first node to the last node
+ * @stk: stack
+ * @len: number of lines in the stack
+*/
+void rotate_l(stack_t **stk, __attribute__((unused))unsigned int len)
+{
+	stack_t *temp;
+
+	/* check if the stack is empty or conatins only one element */
+	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
+	{
+		return;
+	}
+	/* assign stack to temporary stack*/
+	temp = *stk;
+	/* loop through the stack to get last element */
+	while (temp->next != NULL)
+		/* assign the value of last element to temp */
+		temp = temp->next;
+	temp->next = *stk;
+	(*stk)->prev = temp;
+	*stk = (*stk)->next;
+	(*stk)->prev->next = NULL;
+	(*stk)->prev = NULL;
+}
+
+/**
+ * rotate_r - change the last node of the stack to be at top
+ * @stk: stack
+ * @len: number of lines
+ * Return: void
+*/
+void rotate_r(stack_t **stk, __attribute__((unused))unsigned int len)
+{
+	stack_t *temp;
+
+	/* check if empty or contains only one value */
+	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
+		return;
+	temp = *stk;
+	/* loop to get last element in stack */
+	while (temp->next != NULL)
+		temp = temp->next;
+	/* swap */
+	temp->next = *stk;
+	temp->prev->next = NULL;
+	temp->prev = NULL;
+	(*stk)->prev = temp;
+	(*stk) = temp;
+}
