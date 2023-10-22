bd073a55e805fe4b788699151822591355fd68ff
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-22 14:42:06 +0300

    modified main function

--- main.c
+++ main.c
@@ -10,11 +10,14 @@ stack_t *head = NULL;
 
 int main(int argc, char *argv[])
 {
+	/* checks if there is input from user or not */
 	if (argc != 2)
 	{
+		/* if no input print USAGE: monty file*/
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	/* if there is an input, use open file function to operate the program */
 	open_file(argv[1]);
 	free_nd();
 	return (0);
@@ -27,11 +30,14 @@ int main(int argc, char *argv[])
  */
 stack_t *nd_new(int nx)
 {
+	/* create a stack for nodes to add */
 	stack_t *nd;
-
+	/* allocate memory for stack */
 	nd = malloc(sizeof(stack_t));
+	/* if no memory, raise error */
 	if (nd == NULL)
 		err1(4);
+	/* else, add the node to stack */
 	nd->next = NULL;
 	nd->prev = NULL;
 	nd->n = nx;
@@ -43,11 +49,12 @@ stack_t *nd_new(int nx)
  */
 void free_nd(void)
 {
+	/* create temporary stack for free nodes */
 	stack_t *temp;
-
+	/* if pointer heade points to no nodes, do nothing*/
 	if (head == NULL)
 		return;
-
+	/* iterate through the stack to free all nodes */
 	while (head != NULL)
 	{
 		temp = head;
@@ -64,15 +71,18 @@ void free_nd(void)
  */
 void q_add(stack_t **nw_nod, __attribute__((unused))unsigned int len)
 {
+	/* create temporary stack */
 	stack_t *temp;
-
+	/* check if there a node and raise error if null */
 	if (nw_nod == NULL || *nw_nod == NULL)
 		exit(EXIT_FAILURE);
+	/* if no head, create the node to be the head */
 	if (head == NULL)
 	{
 		head = *nw_nod;
 		return;
 	}
+	/* swap */
 	temp = head;
 	while (temp->next != NULL)
 		temp = temp->next;
