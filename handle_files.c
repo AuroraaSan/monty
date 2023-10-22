5bff7279ebcf5f8affd8c8a00b27869785703ee6
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-22 14:36:38 +0300

    files error modified

--- handle_files.c
+++ handle_files.c
@@ -1,75 +1,90 @@
 #include "monty.h"
 
 /**
- * open_file - open file
- * @f_name: name of the file
+ * open_file - opens a file
+ * @f_name: the file namepath
  * Return: void
-*/
+ */
+
 void open_file(char *f_name)
 {
 	FILE *fd = fopen(f_name, "r");
 
 	if (f_name == NULL || fd == NULL)
 		err1(2, f_name);
+
 	f_read(fd);
 	fclose(fd);
 }
 
+
 /**
- * f_read - read file
- * @fd: file desc pointer
+ * f_read - reads a file
+ * @fd: pointer to file descriptor
  * Return: void
-*/
+ */
+
 void f_read(FILE *fd)
 {
 	int num, fr = 0;
-	char *buf = NULL;
+	char *buff = NULL;
 	size_t ln = 0;
 
-	for (num = 1; getline(&buf, &ln, fd) != -1; num++)
-		fr = line_p(buf, num, fr);
-	free(buf);
+	for (num = 1; getline(&buff, &ln, fd) != -1; num++)
+	{
+		fr = line_p(buff, num, fr);
+	}
+	free(buff);
 }
-/**
- * line_p - separate lines
- * @buf: buffer
- * @num: number of lines
- * @fr: format for storage
- * Return: 0 if stack and 1 if queue
-*/
-int line_p(char *buf, int num, int fr)
-{
-	char *operation_c, *val;
-	const char *dlm = "\n";
 
-	if (buf == NULL)
+
+/**
+ * line_p - Separates each line into tokens to determine
+ * which function to call
+ * @buff: line from the file
+ * @num: line number
+ * @fr:  storage fr. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ * Return: Returns 0 if the opcode is stack. 1 if queue.
+ */
+
+int line_p(char *buff, int num, int fr)
+{
+	char *opcode, *value;
+	const char *delim = "\n ";
+/* raise error when buffer empty*/
+	if (buff == NULL)
 		err1(4);
-	operation_c = strtok(buf, dlm);
-	if (operation_c == NULL)
+
+	opcode = strtok(buff, delim);
+	if (opcode == NULL)
 		return (fr);
-	val = strtok(NULL, dlm);
-	if (strcmp(operation_c, "stack") == 0)
+	value = strtok(NULL, delim);
+/* print  0 when stack or 1 when queue*/
+	if (strcmp(opcode, "stack") == 0)
 		return (0);
-	if (strcmp(operation_c, "queue") == 0)
+	if (strcmp(opcode, "queue") == 0)
 		return (1);
-	f_function(operation_c, val, num, fr);
+
+	f_function(opcode, value, num, fr);
 	return (fr);
 }
 
 /**
- * f_function - gets the function needed for the operation
- * @operation_c: opcode
- * @val: arg value
- * @fr: format for storage
- * @num:number line
+ * f_function - find the appropriate function for the opcode
+ * @opcode: opcode
+ * @val: argument of opcode
+ * @fr:  storage fr. If 0 Nodes will be entered as a stack.
+ * @num: line number
+ * if 1 nodes will be entered as a queue.
  * Return: void
-*/
-void f_function(char *operation_c, char *val, int num, int fr)
+ */
+void f_function(char *opcode, char *value, int ln, int fr)
 {
 	int x;
-	int fg;
-
-	instruction_t list_f[] = {
+	int flag;
+/* list of all function used in program*/
+	instruction_t f_list[] = {
 		{"push", stack_add},
 		{"pall", stk_print},
 		{"pint", top_p},
@@ -87,56 +102,59 @@ void f_function(char *operation_c, char *val, int num, int fr)
 		{"rotr", rotate_r},
 		{NULL, NULL}
 	};
-	if (operation_c[0] == '#')
+/* if comment igonre it*/
+	if (opcode[0] == '#')
 		return;
-	for (fg = 1, x = 0; list_f[x].opcode != NULL; x++)
+/* iterate to see which function to call */
+	for (flag = 1, x = 0; f_list[x].opcode != NULL; x++)
 	{
-		if (strcmp(operation_c, list_f[x].opcode) == 0)
+		if (strcmp(opcode, f_list[x].opcode) == 0)
 		{
-			f_call(list_f[x].f, operation_c, val, num, fr);
-			fg = 0;
+			f_call(f_list[x].f, opcode, value, ln, fr);
+			flag = 0;
 		}
 	}
-	if (fg == 1)
-		err1(3, num, operation_c);
+	if (flag == 1)
+		err1(3, ln, opcode);
 }
-/**
- * f_call - call the function to be operated
- * @fun: a ptr to the function
- * @op_c: operation code
- * @val: value in str
- * @num: number line
- * @fr: format for storage
- * Return: void
-*/
 
-void f_call(op_func fun, char *op_c, char *val, int num, int fr)
+
+/**
+ * f_call - Calls the required function.
+ * @func: Pointer to the function that is about to be called.
+ * @op: string representing the opcode.
+ * @value: string representing a numeric value.
+ * @len: line numeber for the instruction.
+ * @fr: fr specifier. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ */
+void f_call(op_func func, char *op, char *value, int len, int fr)
 {
 	stack_t *nd;
 	int fg;
 	int x;
 
 	fg = 1;
-	if (strcmp(op_c, "push") == 0)
+	if (strcmp(op, "push") == 0)
 	{
-		if (val != NULL && val[0] == '-')
+		if (value != NULL && value[0] == '-')
 		{
-			val = val + 1;
+			value = value + 1;
 			fg = -1;
 		}
-		if (val == NULL)
-			err1(5, num);
-		for (x = 0; val[x] != '\0'; x++)
+		if (value == NULL)
+			err1(5, len);
+		for (x = 0; value[x] != '\0'; x++)
 		{
-			if (isdigit(val[x]) == 0)
-				err1(5, num);
+			if (isdigit(value[x]) == 0)
+				err1(5, len);
 		}
-		nd = nd_new(atoi(val) * fg);
+		nd = nd_new(atoi(value) * fg);
 		if (fr == 0)
-			fun(&nd, num);
+			func(&nd, len);
 		if (fr == 1)
-			q_add(&nd, num);
+			q_add(&nd, len);
 	}
 	else
-		fun(&head, num);
+		func(&head, len);
 }
